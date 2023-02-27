37e7d6dac13747e39eaa4ecc6f2b25613ec47172
surbhi-ladhava <surbhiladhava1@gmail.com>
2023-02-27 12:16:58 +0530

    Initial Commit

---
+++ database/migrations/2023_02_19_115724_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('products', function(Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }    
+}
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
