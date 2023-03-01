c4c2bcb60f7e239be7b3f1394637bc2e46b0f3e0
surbhi-ladhava <surbhiladhava1@gmail.com>
2023-03-01 13:49:05 +0530

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\Routing\Route as RoutingRoute;
@@ -9,8 +8,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/products', 'ProductController');
+Route::apiResource('/products', App\Http\Controllers\ProductController::class);
 
 Route::group(['prefix'=>'products'], function(){
-    Route::apiResource('/{product}/reviews', 'ReviewController');
+    Route::apiResource('/{product}/reviews', App\Http\Controllers\ReviewController::class);
 });
