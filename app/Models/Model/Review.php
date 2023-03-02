f6bf49ded35c0b2928da5d5c4b62af88ba7ac013
Pradyuman_Pandya <pradyuman.pandya5055@gmail.com>
2023-03-02 16:14:28 +0530

    Relationship created

--- app/Models/Model/Review.php
+++ app/Models/Model/Review.php
@@ -2,10 +2,15 @@
 
 namespace App\Models\Model;
 
+use App\Models\Model\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
     use HasFactory;
+    public function product()
+    {
+        return $this->belongsTo(product::class);
+    }
 }
